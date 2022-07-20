9f7aa194a8d7511901a78374f488162d6be1244e
JamieHa8794 <JamieHa8794@gmail.com>
2022-07-20 00:04:54 -0400

    set up express & display brands

--- server.js
+++ server.js
@@ -1,4 +1,50 @@
 const pg = require('pg')
+const express = require('express')
+
+const app = express();
+
+app.get('/', async(req, res, next)=>{
+    try{
+        const response = await client.query('SELECT * FROM Brand;')
+        const brands = response.rows;
+        res.send(`
+            <html>
+                <head>
+                </head>
+                <body>
+                    <h1>
+                        Sneaker World
+                    </h1>
+                    <h2>
+                        Brands
+                    </h2>
+                    <ul>
+                    ${
+                        brands.map(brand =>{
+                            return(`
+                                <li>
+                                    <a href='/brands/${brand.id}'>
+                                        ${brand.name}
+                                    </a>
+                                </li>
+                            `)
+                        }).join('')
+                    }
+                    </ul>
+                </body>
+            </html>
+        `)
+    }
+    catch(err){
+        next(err);
+    }
+})
+
+
+
+
+const port = process.env.PORT || 3000;
+
 
 const client = new pg.Client('postgress://localhost/sneaker_world_db');
 
@@ -35,6 +81,7 @@ const setUp = async()=>{
        await client.connect();
        await syncAndSeed();
        console.log('connected to db')
+       app.listen(port, () => console.log(`listening on port ${port}`))
     }
     catch(err){
         console.log(err)
